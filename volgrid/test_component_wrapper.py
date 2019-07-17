b402044764caea6a33e5042e5a9d3788bba3a8c6
henry perlman <bperlman1@gmail.com>
2019-07-17 17:56:59 -0400

    tester ipynb's

--- volgrid/test_component_wrapper.py
+++ volgrid/test_component_wrapper.py
@@ -1,5 +1,6 @@
 import dash
 import sys,os
+from dashgrid.dgrid import ComponentWrapper
 if  not os.path.abspath('./') in sys.path:
     sys.path.append(os.path.abspath('./'))
 if  not os.path.abspath('../') in sys.path:
@@ -15,8 +16,8 @@ dash_list = dgrid.component_example_dataframe_graph().component_list
 clist = [dgrid.ComponentWrapper.build_from_json(cj) for cj in dash_list]
 
 app = dash.Dash()
+
 app.layout = html.Div([c.html for c in clist])
 [c.callback(app) for c in clist if len(c.callback_inputs)>0]
 app.run_server(host='127.0.0.1',port=8600)
-
 
